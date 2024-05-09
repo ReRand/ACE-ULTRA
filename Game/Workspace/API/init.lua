dc00030aca8cf3fd65c27aa394ef005f81a7636a
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-05-09 12:06:11 -0500

    Update init.lua

--- Game/Workspace/API/init.lua
+++ Game/Workspace/API/init.lua
@@ -1,5 +1,17 @@
-local API = {};
+local API = {
+
+  Classes = {}
+  
+};
 API.__index = API;
 
 
-return API;
+
+if not _G.API then
+  _G.API = API;
+  return API;
+  
+else
+  return _G.API;
+
+end
