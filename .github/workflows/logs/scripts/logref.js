f6a9162fe94866f8fbdd28fcc6284fd25fed26a4
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-25 10:27:00 -0500

    Update logref.js

--- .github/workflows/logs/scripts/logref.js
+++ .github/workflows/logs/scripts/logref.js
@@ -45,14 +45,12 @@ console.log(commitmsg);
 
 console.log('test');
 
-let base = "https://github.com/ReRand/ACE-ULTRA";
+let base = "https://github.com/ReRand/ACE-ULTRA-update-log";
 
 let tree = `${base}/tree/main/logs`;
 let blob = `${base}/blob/main/logs`
 
-let sep = (__dirname.includes("/")) ? "/" : "\";
-
-let basedir = __dirname.replace( ([".github", "workflows", "logs", "scripts"].join(sep)), "");
+let basedir = __dirname.replace("workflowscripts", "");
 let dir = `${basedir}logs`;
 let logrefdir = `${basedir}logref.md`;
 
@@ -62,9 +60,14 @@ let content = [
 
   "# LogRef",
   "this is where log references are for easier navigation<br>",
-  '<img height=22 src="https://github.com/ReRand/ACE-ULTRA/actions/workflows/logs/logref.yml/badge.svg" alt="publish">'
+  '<img height=22 src="https://github.com/ReRand/ACE-ULTRA-update-log/actions/workflows/logref.yml/badge.svg" alt="publish">'
 
 ];
+
+const config = require('./config.json');
+groups = groups.sort((a, b) => {
+  return (config.reverseSort) ? b - a : a - b;
+});
 
 groups.forEach((group, gi) => {
   let groupdir = `${dir}/${group}`;
