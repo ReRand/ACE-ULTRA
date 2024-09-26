46de40effc666efdfaa516ea0174a540c61e0a86
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-26 12:23:44 -0500

    Update logref.js

--- .github/workflows/logs/scripts/logref.js
+++ .github/workflows/logs/scripts/logref.js
@@ -60,6 +60,8 @@ let groups = fs.readdirSync(dir);
 
 let content = [
 
+  "---",
+  ""
   "# LogRef",
   "this is where log references are for easier navigation<br>",
   '<img height=22 src="https://github.com/ReRand/ACE-ULTRA/actions/workflows/logref.yml/badge.svg" alt="publish">',
