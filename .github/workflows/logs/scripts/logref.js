cf479929ccde47291b04835f9e36147ee655a444
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-26 11:42:10 -0500

    Update logref.js

--- .github/workflows/logs/scripts/logref.js
+++ .github/workflows/logs/scripts/logref.js
@@ -129,7 +129,7 @@ versionnames.forEach((vn, i) => {
     stuff.unshift(versions[vn].header);
 
     vsubs = vsubs.sort( (a, b) => {
-      console.log(a, b);
+      console.log(a.localecompare(b));
       return a - b;
     });
     
